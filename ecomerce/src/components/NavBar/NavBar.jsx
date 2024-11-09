7779206b83efa1cf1aae0e094a294cbba01aa2d6
julian <julianpalacios718@gmail.com>
2024-11-09 16:55:24 -0300

    .

--- ecomerce/src/components/NavBar/NavBar.jsx
+++ ecomerce/src/components/NavBar/NavBar.jsx
@@ -3,40 +3,28 @@ function NavBar() {
 
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="./index.html">Mi Tienda</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="./index.html">Inicio</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="./pages/products.html">Productos</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Nosotros
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="./pages/weAre.html">Quiénes somos</a></li>
-                                <li><a className="dropdown-item" href="./pages/login.html">Usuarios</a></li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="./pages/contactUs.html">Contáctanos</a>
-                        </li>
-                        <li className="nav-item">
-                           
-                            <a className="nav-link" href="./pages/shoppingCart.html"><i className="bi bi-cart3"></i> Carrito</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Tu Marca</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Colección</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" onclick="toggleCart()">Carrito <span id="cart-count" class="badge badge-light">0</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
 }
 
